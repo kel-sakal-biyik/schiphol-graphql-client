2e7ecc3dfd90f33cfb967a2e2cb7c1f770d2d0d8
Ugur Korfali <ugur.korfali@klm.com>
2018-02-18 15:18:08 +0100

    initial commit

--- src/app/flight-list/flight-list.component.ts
+++ src/app/flight-list/flight-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-flight-list',
@@ -8,21 +10,39 @@ import { Component, OnInit } from '@angular/core';
 export class FlightListComponent implements OnInit {
   public flightData: any;
 
-  constructor() {}
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
-    this.flightData = [
-      {
-        scheduleTime: '00:05',
-        flightName: 'KL2598',
-        airlineConnection: {
-          publicName: 'KLM'
-        },
-        destinationConnection: {
-          city: 'Sofia',
-          country: 'Bulgaria'
+    const date = new Date();
+    const scheduleDate = [
+      date.getFullYear(),
+      `0${(date.getMonth() + 1)}`.slice(-2),
+      `0${(date.getDate())}`.slice(-2),
+    ].join('-');
+    const scheduleTime = `${date.getHours()}:${date.getMinutes()}`;
+    const flightListQuery = gql`
+      query FlightListQuery($scheduleDate: String, $scheduleTime: String) {
+        flights(scheduleDate: $scheduleDate, scheduleTime: $scheduleTime) {
+          flightName
+          scheduleTime
+          destinationConnection {
+            city
+            country
+          }
+          airlineConnection {
+            publicName
+          }
         }
       }
-    ]
+    `;
+
+    this.flightData = this.apollo.watchQuery({
+      query: flightListQuery,
+      variables: {
+        scheduleDate,
+        scheduleTime
+      },
+      fetchPolicy: 'cache-first'
+    });
   }
 }
